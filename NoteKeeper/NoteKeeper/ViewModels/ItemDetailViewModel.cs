a3057cfac78260f41b422b76af5e1d34062bdb18
DhruvKinger <cse1785@gmail.com>
2020-07-17 00:31:22 +0530

    added files

---
+++ NoteKeeper/NoteKeeper/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using NoteKeeper.Models;
+
+namespace NoteKeeper.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
